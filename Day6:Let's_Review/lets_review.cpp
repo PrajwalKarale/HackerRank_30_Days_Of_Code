7018181d5e6c7cc62c2bcd04a75d0c45335ef40c
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2020-06-04 15:15:06 +0530

    Update lets_review.cpp

--- Day6:Let's_Review/lets_review.cpp
+++ Day6:Let's_Review/lets_review.cpp
@@ -1,1 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include<bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string input;
+        string even_result = "";
+        string odd_result = "";
+        cin >> input;
+        // cout << input;
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(i % 2 == 0)
+            {
+                even_result = even_result + input[i];
+            }
+            else
+            {
+                odd_result = odd_result + input[i];
+            }
+        }
+        cout << even_result << " " << odd_result << endl;
+    }   
+    return 0;
+}
