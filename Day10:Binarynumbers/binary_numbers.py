ccbd5bb3a054821915770b9d3b57c385258efb28
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2020-06-08 23:42:44 +0530

    Update binary_numbers.py

--- Day10:Binarynumbers/binary_numbers.py
+++ Day10:Binarynumbers/binary_numbers.py
@@ -1,1 +1,8 @@
-
+def maxConsecutive1(input):  
+     input = bin(input)  
+     input = input[2:]  
+     print (max(map(len, input.split('0')))) 
+# Driver program 
+if __name__ == '__main__': 
+    input = int(input())
+    maxConsecutive1(input) 
